bd5655fa3df2cdd63a2a9fd4564bd53239d1bc84
Amritanshu Mukund <amukund007@gmail.com>
2025-11-20 16:01:06 +0530

    Enhance task completion loop in timer
    Refactor advanced timer functionality to repeat task prompts until completion.

--- To_do_list.py
+++ To_do_list.py
@@ -1,16 +1,6 @@
-# # timer list
-# import time
-# a=input("Choose your task or type exit to end = ")
-# b=int(input("time alloted= "))
-# i=b
-# print("do your task",a)
-# while i>=0:
-#     time.sleep(1)
-#     print(i)
-#     i-=1
-# print ("task complete")
+# advanced timer list in which until you don't want to stop it can ask for task on its own
+# until you dont complete your task it will ask you to do again
 
-# advanced timer list
 c=input("enter s to start or e to exit= ").lower()
 while c=="s":
     import time
@@ -34,5 +24,17 @@ while c=="s":
     elif d=="no":
         print("Do the task again")
     else:
-        print("invalid input")    
+        print("invalid input")  
+    while d=="no":
+         x=int(input("Choose the countdown time(in hours) "))
+         y=int(input("Choose the countdown time(in minutes) "))
+         z=int(input("Choose the countdown time(in seconds) "))
+         i=x*3600+y*60+z
+         print("do your task",a)
+         while i>=0:
+            time.sleep(1)
+            print(i)
+            i-=1
+         d=input("Did you completed task, YES OR NO = ").lower()
 print("exited")
+
